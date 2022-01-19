59f7811d3350d9ef7b762cbbce10f4f3f2c11adb
karimtito <karim.tit@ensae-paristech.fr>
2022-01-19 14:05:50 +0100

    fixing arguments

--- linear_gaussian_test_langevin_base_pyt.py
+++ linear_gaussian_test_langevin_base_pyt.py
@@ -8,8 +8,10 @@ import os
 import torch
 import GPUtil
 import cpuinfo
+import pandas as pd
 import argparse
 from dev.utils import str2bool,str2floatList,str2intList
+
 
 method_name="langevin_base_pyt"
 
@@ -23,26 +25,31 @@ class config:
     p_range=[1e-15]
     n_rep=10
     mh_opt=False
-    
-
+    save_config=False 
+    d=5
+    verbose=1
+    log_dir='./logs/linear_gaussian'
+    sigma=1
+    v1_kernel=False
+    torch_seed=0
+    gpu_name=None
 
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--log_dir',default=config.log_dir)
 parser.add_argument('--n_rep',type=int,default=config.n_rep)
-parser.add_argument('--N',type=int,default=config.N)
+#parser.add_argument('--N',type=int,default=config.N)
 parser.add_argument('--verbose',type=float,default=config.verbose)
 parser.add_argument('--d',type=int,default=config.d)
 parser.add_argument('--min_rate',type=float,default=config.min_rate)
-parser.add_argument('--alpha',type=float,default=config.alpha)
+#parser.add_argument('--alpha',type=float,default=config.alpha)
 parser.add_argument('--n_max',type=int,default=config.n_max)
 parser.add_argument('--tqdm_opt',type=str2bool,default=config.tqdm_opt)
-parser.add_argument('--T',type=int,default=config.T)
+#parser.add_argument('--T',type=int,default=config.T)
 parser.add_argument('--save_config',type=str2bool, default=config.save_config)
-parser.add_argument('--update_agg_res',type=str2bool,default=config.update_agg_res)
-parser.add_argument('--aggr_res_path',type=str, default=config.aggr_res_path)
-parser.add_argument('--rho',type=float,default=config.rho)
-parser.add_argument('--gaussian_latent',type=str2bool, default=config.gaussian_latent)
+#parser.add_argument('--update_agg_res',type=str2bool,default=config.update_agg_res)
+#parser.add_argument('--aggr_res_path',type=str, default=config.aggr_res_path)
+#parser.add_argument('--rho',type=float,default=config.rho)
 parser.add_argument('--allow_multi_gpu',type=str2bool)
 parser.add_argument('--g_target',type=float,default=config.g_target)
 parser.add_argument('--track_gpu',type=str2bool,default=config.track_gpu)
@@ -51,23 +58,15 @@ parser.add_argument('--device',type=str, default=config.device)
 parser.add_argument('--allow_zero_est',type=str2bool, default=config.allow_zero_est)
 parser.add_argument('--torch_seed',type=int, default=config.torch_seed)
 parser.add_argument('--np_seed',type=int, default=config.np_seed)
-parser.add_argument('--export_to_onnx',type=str2bool, default=config.export_to_onnx)
-parser.add_argument('--use_attack',type=str2bool,default=config.use_attack)
-parser.add_argument('--attack',type=str,default=config.attack)
 parser.add_argument('--epsilons',type=str2floatList,default=config.epsilons)
-parser.add_argument('--input_index',type=int,default=config.input_index)
-parser.add_argument('--lirpa_bounds',type=str2bool, default=config.lirpa_bounds)
-parser.add_argument('--eps_min',type=float, default=config.eps_min)
-parser.add_argument('--eps_max',type=float, default=config.eps_max)
-parser.add_argument('--eps_num',type=int,default=config.eps_num)
-parser.add_argument('--train_model',type=str2bool,default=config.train_model)
 parser.add_argument('--noise_dist',type=str, default=config.noise_dist)
-parser.add_argument('--a',type=float, default=config.a)
+parser.add_argument('--sigma', type=float,default=config.sigma)
+
 parser.add_argument('--N_range',type=str2intList,default=config.N_range)
 parser.add_argument('--T_range',type=str2intList,default=config.T_range)
 parser.add_argument('--rho_range', type=str2floatList,default=config.rho_range)
 parser.add_argument('--alpha_range',type=str2floatList,default=config.alpha_range)
-parser.add_argument('--download',type=str2bool, default=config.download)
+parser.add_argument('--v1_kernel',type=str2bool,default=config.v1_kernel)
 args=parser.parse_args()
 
 for k,v in vars(args).items():
@@ -82,22 +81,13 @@ if config.noise_dist not in ['uniform','gaussian']:
 if not config.allow_multi_gpu:
     os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-if config.np_seed is None:
-    config.np_seed=int(time.time())
-np.random.seed(seed=config.np_seed)
+
 
 if config.torch_seed is None:
     config.torch_seed=int(time.time())
 torch.manual_seed(seed=config.torch_seed)
 
-if len(config.T_range)<1:
-    config.T_range=[config.T]
-if len(config.N_range)<1:
-    config.N_range=[config.N]
-if len(config.rho_range)<1:
-    config.rho_range=[config.rho]
-if len(config.alpha_range)<1:
-    config.alpha_range=[config.alpha]
+
 
 if config.track_gpu:
     gpus=GPUtil.getGPUs()
@@ -127,6 +117,11 @@ if not os.path.exists('./logs'):
 if not os.path.exists(config.log_dir):
     os.mkdir(config.log_dir)
 
+if os.path.exists('../logs/linear_gaussian_tests/results.csv'):
+    results_g=pd.read_csv('../logs/linear_gaussian_tests/results.csv')
+else:
+    results_g=pd.DataFrame(columns=['p_t','mean_est','mean_time','mean_err','std_time','std_est','T','N','rho','alpha','n_rep','min_rate','method'])
+
 raw_logs_path=os.path.join(config.log_dir,'raw_logs')
 if not os.path.exists(raw_logs_path):
     os.mkdir(raw_logs_path)
@@ -152,9 +147,9 @@ mh_str="adjusted" if config.mh_opt else "unadjusted"
 method="langevin_base_"+mh_str
 save_every = 1
 d=5
-v1_kernel=False
-kernel_str='v1_kernel' if v1_kernel else 'v2_kernel'
-kernel_function=t_u.langevin_kernel_pyt if v1_kernel else t_u.langevin_kernel_pyt2 
+
+kernel_str='v1_kernel' if config.v1_kernel else 'v2_kernel'
+kernel_function=t_u.langevin_kernel_pyt if config.v1_kernel else t_u.langevin_kernel_pyt2 
 get_c_norm= lambda p:stat.norm.isf(p)
 run_nb=0
 iterator= tqdm(range(config.n_rep))
@@ -199,7 +194,8 @@ for p_t in config.p_range:
                     results_g=results_g.append({'p_t':p_t,'mean_est':ests.mean(),'mean_err':errs.mean(),
                     'mean_time':times.mean(),'std_time':times.std(),'std_est':ests.std(),'T':T,'N':N,
                     'rho':rho,'alpha':alpha,'n_rep':config.n_rep,'min_rate':config.min_rate,'d':d,
-                    "method":method,"kernel":kernel_str,"finish_rate":fin.mean()},ignore_index=True)
+                    "method":method,"kernel":kernel_str,"finish_rate":fin.mean(),
+                    'gpu_name':config.gpu_name},ignore_index=True)
                     
                     if run_nb%save_every==0:
                         results_g.to_csv('../logs/linear_gaussian_tests/results.csv',index=False)
