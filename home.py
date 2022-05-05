b764eb4018452d0abe358a800ef9232dbb64acc2
karimtito <karim.tit@ensae-paristech.fr>
2022-05-05 11:53:21 +0200

    add home file

---
+++ home.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_DIR=os.path.dirname(os.path.realpath(__file__))
+DATA_PATH=os.path.join(ROOT_DIR,'data')
+LOG_PATH=os.path.join(ROOT_DIR,'logs')
+MODELS_PATH=os.path.join(ROOT_DIR,'models')
