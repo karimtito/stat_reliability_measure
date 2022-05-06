fix potential computation with uniform noise

dev/torch_utils.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 from stat_reliability_measure.dev.utils import float_to_file_float
-from stat_reliability_measure.dev.torch_arch import CNN_custom,dnn2,dnn4,LeNet
+from stat_reliability_measure.dev.torch_arch import CNN_custom,dnn2,dnn4,LeNet,ConvNet
 from torchvision import transforms,datasets,models as tv_models
 from torch.utils.data import DataLoader
 import timm
@@ -313,40 +313,40 @@ def compute_h_pyt(model, input_, target_class):
 
 normal_dist=torch.distributions.Normal(loc=0, scale=1.)
 
-def V_pyt(x_,x_0,model,target_class,epsilon=0.05,gaussian_latent=True,clipping=True, clip_min=0, clip_max=1,reshape=True,input_shape=None):
+def V_pyt(x_,x_0,model,target_class,low,high,gaussian_latent=True,reshape=True,input_shape=None):
     with torch.no_grad():
         if input_shape is None:
             input_shape=x_0.shape
         if gaussian_latent:
-            u=epsilon*(2*normal_dist.cdf(x_)-1)
+            u=normal_dist.cdf(x_)
         else:
             u=x_
         if reshape:
             u=torch.reshape(u,(u.shape[0],)+input_shape)
 
         
-        x_p = torch.clamp(x_0+u,min=clip_min,max=clip_max) if clipping else x_0+u
+        x_p = low+(high-low)*u
     v = compute_V_pyt(model=model,input_=x_p,target_class=target_class)
     return v
 
-def gradV_pyt(x_,x_0,model,target_class,epsilon=0.05,gaussian_latent=True,clipping=True, clip_min=0, clip_max=1,reshape=True,input_shape=None):
+def gradV_pyt(x_,x_0,model,target_class,low,high,gaussian_latent=True,reshape=True,input_shape=None):
    
     if input_shape is None:
         input_shape=x_0.shape
     if gaussian_latent:
-        u=epsilon*(2*normal_dist.cdf(x_)-1)
+        u=normal_dist.cdf(x_)
     else:
         u=x_
     if reshape:
         u=torch.reshape(u,(u.shape[0],)+input_shape)
     
-    x_p = torch.clamp(x_0+u,min=clip_min,max=clip_max) if clipping else x_0+u
+    x_p = low+(high-low)*u
     _,grad_u = compute_V_grad_pyt(model=model,input_=x_p,target_class=target_class)
     grad_u=torch.reshape(grad_u,x_.shape)
     if gaussian_latent:
-        grad_x=torch.exp(normal_dist.log_prob(x_))*grad_u*(2*epsilon)
+        grad_x=torch.exp(normal_dist.log_prob(x_))*(high-low)*grad_u
     else:
-        grad_x=grad_u
+        grad_x=(high-low)*grad_u
     return grad_x
 
 def correct_min_max(x_min,x_max,x_mean,x_std):

tests/imagenet_tests/imagenet_test_smc.py
@@ -376,11 +376,11 @@ X_correct,label_correct,accuracy=t_u.get_correct_x_y(data_loader=test_loader,dev
 if config.verbose>=2:
     print(f"model accuracy on test batch:{accuracy}")
 
-x_min=torch.tensor(0)
-x_max=torch.tensor(1)
+x_min=0
+x_max=1
 
 if config.use_attack:
-    fmodel = fb.PyTorchModel(model, bounds=(0,1),device=device)
+    fmodel = fb.PyTorchModel(model, bounds=(x_min,x_max),device=device)
     attack=fb.attacks.LinfPGD()
     #un-normalize data before performing attack
     #epsilons= np.array([0.0, 0.001, 0.01, 0.03,0.04,0.05,0.07,0.08,0.0825,0.085,0.086,0.087,0.09, 0.1, 0.3, 0.5, 1.0])
@@ -395,9 +395,6 @@ run_nb=0
 iterator= tqdm(range(config.n_rep))
 exp_res=[]
 model_name=config.model_arch
-clip_min=torch.tensor(x_min).to(device)
-
-clip_max=torch.tensor(x_max).to(device)
 for l in inp_indices:
     with torch.no_grad():
     
@@ -429,10 +426,10 @@ for l in inp_indices:
             gen = lambda N: torch.randn(size=(N,d),device=config.device)
         else:
             gen= lambda N: (2*torch.rand(size=(N,d), device=device )-1)
-        V_ = lambda X: t_u.V_pyt(X,x_0=x_0,model=model,epsilon=epsilon, target_class=y_0,gaussian_latent=config.gaussian_latent,clip_min=clip_min,
-        clip_max=clip_max)
-        gradV_ = lambda X: t_u.gradV_pyt(X,x_0=x_0,model=model, target_class=y_0,epsilon=epsilon, gaussian_latent=config.gaussian_latent,
-        clip_min=clip_min,clip_max=clip_max)
+        low=torch.max(x_0-epsilon, torch.tensor([x_min]).cuda())
+        high=torch.min(x_0+epsilon, torch.tensor([x_max]).cuda())  
+        V_ = lambda X: t_u.V_pyt(X,x_0=x_0,model=model,low=low,high=high,target_class=y_0,gaussian_latent=config.gaussian_latent)
+        gradV_ = lambda X: t_u.gradV_pyt(X,x_0=x_0,model=model,low=low,high=high, target_class=y_0,gaussian_latent=config.gaussian_latent)
         for ess_t in config.e_range:
             if config.adapt_func.lower()=='ess':
                 adapt_func = lambda beta,v : smc_pyt.nextBetaESS(beta_old=beta,v=v,ess_alpha=ess_t,max_beta=1e6)

tests/mnist_cifar_tests/mnist_cifar_test_amls.py
@@ -339,23 +339,19 @@ for l in range(len(inp_indices)):
         if config.gaussian_latent:
             amls_gen = lambda N: torch.randn(size=(N,d),device=config.device)
         else:
-            amls_gen= lambda N: (2*torch.rand(size=(N,d), device=device )-1)
-        
-        
-        
-        amls_gen = lambda N: torch.randn(size=(N,d),device=config.device)
+            amls_gen= lambda N: torch.rand(size=(N,d), device=device )
         normal_kernel =  lambda x,s : (x + s*torch.randn(size = x.shape,device=config.device))/np.sqrt(1+s**2) #normal law kernel, appliable to vectors 
-        
-            
-        def h(x,gaussian_latent=config.gaussian_latent,eps=epsilon,x_min=x_min,x_max=x_max):
+        low=torch.max(x_0-epsilon, torch.tensor([x_min]).cuda())
+        high=torch.min(x_0+epsilon, torch.tensor([x_max]).cuda())            
+        def h(x,gaussian_latent=config.gaussian_latent,low=low,high=high):
             x_m=x.reshape((x.shape[0],)+input_shape)
             if gaussian_latent:
-                x_m=2*normal_dist.cdf(x_m)-1
+                x_m=normal_dist.cdf(x_m)
             with torch.no_grad():
 
-                input_m=torch.clamp(x_0+eps*x_m, min=x_min,max=x_max)
+                x_m=low+(high-low)*x_m
                 
-                y = model(input_m)
+                y = model(x_m)
                 y_diff = torch.cat((y[:,:y_0], y[:,(y_0+1):]),dim=1) - y[:,y_0].unsqueeze(-1)
                 y_diff, _ = y_diff.max(dim=1)
             return y_diff #.max(dim=1)

tests/mnist_cifar_tests/mnist_cifar_test_smc.py
@@ -430,10 +430,11 @@ for l in inp_indices:
             gen = lambda N: torch.randn(size=(N,d),device=config.device)
         else:
             gen= lambda N: (2*torch.rand(size=(N,d), device=device )-1)
-        V_ = lambda X: t_u.V_pyt(X,x_0=x_0,model=model,epsilon=epsilon, target_class=y_0,gaussian_latent=config.gaussian_latent,clip_min=clip_min,
-        clip_max=clip_max)
-        gradV_ = lambda X: t_u.gradV_pyt(X,x_0=x_0,model=model, target_class=y_0,epsilon=epsilon, gaussian_latent=config.gaussian_latent,
-        clip_min=clip_min,clip_max=clip_max)
+        low=torch.max(x_0-epsilon, torch.tensor([x_min]).cuda())
+        high=torch.min(x_0+epsilon, torch.tensor([x_max]).cuda())  
+        V_ = lambda X: t_u.V_pyt(X,x_0=x_0,model=model,low=low,high=high,target_class=y_0,gaussian_latent=config.gaussian_latent)
+        gradV_ = lambda X: t_u.gradV_pyt(X,x_0=x_0,model=model,low=low,high=high, target_class=y_0,gaussian_latent=config.gaussian_latent)
+        
         for ess_t in config.e_range:
             if config.adapt_func.lower()=='ess':
                 adapt_func = lambda beta,v : smc_pyt.nextBetaESS(beta_old=beta,v=v,ess_alpha=ess_t,max_beta=1e6)