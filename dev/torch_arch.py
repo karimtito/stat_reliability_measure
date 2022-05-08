fd6e5c3791fdc68f75bfe94c7b686659b1e8a081
karimtito <karim.tit@ensae-paristech.fr>
2022-05-08 11:29:05 +0200

    add densenet arch

--- dev/torch_arch.py
+++ dev/torch_arch.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch.functional as F
 import numpy as np
+import math
+import torch
 
 
 datasets_in_shape={'mnist':(1,28,28),'cifar10':(3,32,32),'cifar100':(3,32,32)}
@@ -70,7 +72,7 @@ class CNN_custom(nn.Module):
         return out
 
 #####################################################################################
-#######   CIFAR10/CIFAR100 models
+#######   CIFAR10 models
 #######
 #####################################################################################
 
@@ -126,39 +128,172 @@ class ConvNet(nn.Module):
         x = self.fc3(x)
         return x
 
-#model_dnn_2.load_state_dict(torch.load("model_dnn_2.pt"))
-#model_dnn_4.load_state_dict(torch.load("model_dnn_4.pt"))
-# #model_cnn.load_state_dict(torch.load("model_cnn.pt"))
 
-# class CNN(nn.Module):
-#     def __init__(self, block, num_blocks, num_classes=10, in_planes=64):
-#         super(CNN, self).__init__()
-#         self.in_planes = in_planes
+#####################################################################################
+#######   CIFAR100 model
+#######
+#####################################################################################
 
-#         self.conv1 = nn.Conv2d(3, in_planes, kernel_size=3,
-#                                stride=1, padding=1, bias=False)
-#         self.bn1 = nn.BatchNorm2d(in_planes)
-#         self.layer1 = self._make_layer(block, in_planes, num_blocks[0], stride=1)
-#         self.layer2 = self._make_layer(block, in_planes * 2, num_blocks[1], stride=2)
-#         self.layer3 = self._make_layer(block, in_planes * 4, num_blocks[2], stride=2)
-#         self.layer4 = self._make_layer(block, in_planes * 8, num_blocks[3], stride=2)
-#         self.linear = nn.Linear(in_planes * 8 * block.expansion, num_classes)
 
-#     def _make_layer(self, block, planes, num_blocks, stride):
-#         strides = [stride] + [1]*(num_blocks-1)
-#         layers = []
-#         for stride in strides:
-#             layers.append(block(self.in_planes, planes, stride))
-#             self.in_planes = planes * block.expansion
-#         return nn.Sequential(*layers)
 
-#     def forward(self, x):
-#         out = F.relu(self.bn1(self.conv1(x)))
-#         out = self.layer1(out)
-#         out = self.layer2(out)
-#         out = self.layer3(out)
-#         out = self.layer4(out)
-#         out = F.avg_pool2d(out, 4)
-#         out = out.view(out.size(0), -1)
-#         out = self.linear(out)
-#         return out
+"""
+Code from https://github.com/andreasveit/densenet-pytorch/blob/master/densenet.py
+BSD 3-Clause License
+Copyright (c) 2017, Andreas Veit
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, dropRate=0.0):
+        super(BasicBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.droprate = dropRate
+
+    def forward(self, x):
+        out = self.conv1(self.relu(self.bn1(x)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, training=self.training)
+        return torch.cat([x, out], 1)
+
+
+class BottleneckBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, dropRate=0.0):
+        super(BottleneckBlock, self).__init__()
+        inter_planes = out_planes * 4
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, inter_planes, kernel_size=1,
+                               stride=1, padding=0, bias=False)
+        self.bn2 = nn.BatchNorm2d(inter_planes)
+        self.conv2 = nn.Conv2d(inter_planes, out_planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.droprate = dropRate
+
+    def forward(self, x):
+        out = self.conv1(self.relu(self.bn1(x)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, inplace=False,
+                            training=self.training)
+        out = self.conv2(self.relu(self.bn2(out)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, inplace=False,
+                            training=self.training)
+        return torch.cat([x, out], 1)
+
+
+class TransitionBlock(nn.Module):
+    def __init__(self, in_planes, out_planes, dropRate=0.0):
+        super(TransitionBlock, self).__init__()
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1,
+                               padding=0, bias=False)
+        self.droprate = dropRate
+
+    def forward(self, x):
+        out = self.conv1(self.relu(self.bn1(x)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, inplace=False,
+                            training=self.training)
+        return F.avg_pool2d(out, 2)
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, nb_layers, in_planes, growth_rate, block, dropRate=0.0):
+        super(DenseBlock, self).__init__()
+        self.layer = self._make_layer(block, in_planes, growth_rate,
+                                      nb_layers, dropRate)
+
+    def _make_layer(self, block, in_planes, growth_rate, nb_layers, dropRate):
+        layers = []
+        #print('nb_layers', nb_layers)
+        for i in range(int(nb_layers)):
+            layers.append(block(in_planes + i * growth_rate,
+                                growth_rate, dropRate))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+class DenseNet3(nn.Module):
+    def __init__(self, depth, num_classes, growth_rate=12,
+                 reduction=0.5, bottleneck=True, dropRate=0.0):
+        super(DenseNet3, self).__init__()
+        in_planes = 2 * growth_rate
+        n = (depth - 4) / 3
+
+        if bottleneck is True:
+            n = n / 2
+            block = BottleneckBlock
+        else:
+            block = BasicBlock
+        # 1st conv before any dense block
+        self.conv1 = nn.Conv2d(3, in_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        # 1st block
+        self.block1 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
+        in_planes = int(in_planes + n * growth_rate)
+        self.trans1 = TransitionBlock(in_planes,
+                                      int(math.floor(in_planes * reduction)),
+                                      dropRate=dropRate)
+        in_planes = int(math.floor(in_planes * reduction))
+        # 2nd block
+        self.block2 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
+        in_planes = int(in_planes + n * growth_rate)
+        self.trans2 = TransitionBlock(in_planes,
+                                      int(math.floor(in_planes * reduction)),
+                                      dropRate=dropRate)
+        in_planes = int(math.floor(in_planes * reduction))
+        # 3rd block
+        self.block3 = DenseBlock(n, in_planes, growth_rate, block, dropRate)
+        in_planes = int(in_planes + n * growth_rate)
+        # global average pooling and classifier
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc = nn.Linear(in_planes, num_classes)
+        self.in_planes = in_planes
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.trans1(self.block1(out))
+        out = self.trans2(self.block2(out))
+        out = self.block3(out)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, 8)
+        out = out.view(-1, self.in_planes)
+        return self.fc(out)
