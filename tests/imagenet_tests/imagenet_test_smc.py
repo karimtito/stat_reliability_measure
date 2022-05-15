359c289571cb6b0d4156e5e05753f3bed69cfb13
karimtito <karim.tit@ensae-paristech.fr>
2022-05-15 16:21:46 +0200

    correct imagenet test for hmc-smc

--- tests/imagenet_tests/imagenet_test_smc.py
+++ tests/imagenet_tests/imagenet_test_smc.py
@@ -337,6 +337,8 @@ if not os.path.exists(raw_logs_path):
 loc_time= datetime.today().isoformat().split('.')[0]
 log_name=method_name+'_'+'_'+loc_time
 exp_log_path=os.path.join(raw_logs_path,log_name)
+if os.path.exists(exp_log_path):
+    exp_log_path=exp_log_path+str(np.randint(10))
 os.mkdir(path=exp_log_path)
 config.json=vars(args)
 
@@ -439,7 +441,9 @@ for l in inp_indices:
                             loc_time= datetime.today().isoformat().split('.')[0]
                             log_name=method_name+f'_N_{N}_T_{T}_L_{L}_a_{float_to_file_float(alpha)}_ess_{float_to_file_float(ess_t)}'+'_'+loc_time.split('_')[0]
                             log_path=os.path.join(exp_log_path,log_name)
-                            
+                            if os.path.exists(log_path):
+                                log_path=log_path+str(np.randint(10))
+                                
                             
                             os.mkdir(path=log_path)
                             run_nb+=1
