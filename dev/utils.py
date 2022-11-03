eb80962cac2a938d517ed5d77197c6615cf24462
karimtito <karim.tit@ensae-paristech.fr>
2022-11-03 10:41:28 +0100

    add dichotomic search for deacreasing functions

--- dev/utils.py
+++ dev/utils.py
@@ -26,6 +26,31 @@ def dichotomic_search(f, a, b, thresh=0, n_max =50):
 
     return high, f(high)
 
+def dichotomic_search_d(f, a, b, thresh=0, n_max =50):
+    """Implementation of dichotomic search of minimum solution for an decreasing function
+        Args:
+            -f: increasing function
+            -a: lower bound of search space
+            -b: upper bound of search space
+            -thresh: threshold such that if f(x)>=thresh, x is considered to be a solution of the problem
+    
+    """
+    low = a
+    high = b
+     
+    i=0
+    while i<n_max:
+        i+=1
+        if f(low)<=thresh:
+            return low, f(low)
+        mid = 0.5*(low+high)
+        if f(mid)<thresh:
+            high=mid
+        else:
+            low=mid
+
+    return high, f(high)
+
 def float_to_file_float(x):
     x=str(x).replace('.','_').replace(',','_')
     return x
