5b7ee2e89f40095958dbb052c51db6e3b9482495
karimtito <karim.tit@ensae-paristech.fr>
2022-12-01 09:08:17 +0100

    fix tf utils

--- dev/tf_arch.py
+++ dev/tf_arch.py
@@ -1,32 +1,38 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D,Dense
+from tensorflow.keras.layers import Conv2D,Dense,Flatten
 from stat_reliability_measure.dev.tf_utils import dic_in_shape_tf
+
+
 
 class CNN_custom_tf(tf.keras.Model):
     def __init__(self, num_classes=10,dataset='mnist'):
         super().__init__()
-        self.input_shape=dic_in_shape_tf[dataset]
-        self.conv1=Conv2D(filters=32,kernel_size=3,padding="same", input_shape=self.input_shape,
+        self.model_shape=dic_in_shape_tf[dataset]
+        self.conv1=Conv2D(filters=32,kernel_size=3,padding="same", input_shape=self.model_shape,
         activation=tf.nn.relu)
      
-        self.conv2=Conv2D(filters=32,kernel_size=3, padding="same",stride=2, 
+        self.conv2=Conv2D(filters=32,kernel_size=3, padding="same",strides=2, 
         activation=tf.nn.relu)
     
         self.conv3=Conv2D(filters=64,kernel_size=3,padding="same", 
         activation=tf.nn.relu)
-        self.conv4=Conv2D(filters=64,kernel_size=3,padding="same",stride=2,
+        self.conv4=Conv2D(filters=64,kernel_size=3,padding="same",strides=2,
         activation=tf.nn.relu)
-        
+        self.flatten = Flatten()
         self.linear1=Dense(units=100,activation= tf.nn.relu)
         self.linear2=Dense(units=num_classes)
-        Dense()
 
+
+   
     def call(self, x):
         out=self.conv1(x)
         out=self.conv2(out)
         out=self.conv3(out)
         out=self.conv4(out)
-        out=Flatten()(out)
+        out= self.flatten(out)
         out=self.linear1(out)
         out= self.linear2(out)
         return out
+
+
+tf_supp_archs={'mnist':{'cnn_custom':CNN_custom_tf}}
--- dev/tf_utils.py
+++ dev/tf_utils.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
+from stat_reliability_measure.dev.utils import float_to_file_float
+
+import os
 def mult_expand_dims(x,k=2):
     for _ in range(k):
         x= tf.expand_dims(x,- 1)
@@ -7,6 +10,8 @@ def mult_expand_dims(x,k=2):
 
 dic_in_shape_tf={'mnist':(28,28,1),'cifar10':(32,32,3),
 'cifar100':(32,32,3),'imagenet':(224,224,3)}
+
+norm_dist_tf = tfp.distributions.Normal
 
 def compute_V_grad_tf(model, input_, target_class):
     """ Returns potentials and potentials gradients for given input_, model and target classes """
@@ -16,6 +21,7 @@ def compute_V_grad_tf(model, input_, target_class):
         val, ind= tf.math.top_k(logits,k=2)
         output=val[:,0]-val[:,1]
 
+    ind = tf.cast(ind,dtype = tf.int32)
     a_priori_grad=tape.gradient(output,input_)
     mask=tf.equal(ind[:,0],target_class)
     v=tf.where(condition=mask, x=output,y=tf.zeros(output.shape))
@@ -23,13 +29,35 @@ def compute_V_grad_tf(model, input_, target_class):
     grad=tf.where(condition=mask, x=a_priori_grad,y=tf.zeros(a_priori_grad.shape))
     return v,grad
 
+def compute_V_grad_tf2(model, input_, target_class, L=0,v_max =1e5):
+    """ Returns potentials and potentials gradients for given input_, model and target classes """
+    with tf.GradientTape() as tape:
+        tape.watch(input_)
+        y= model(input_) 
+        y_0 =target_class
+        log_diff = tf.concat((y[:,:y_0], y[:,(y_0+1):]),axis=1)-tf.expand_dims(y[:,y_0],-1)
+        score = tf.reduce_max(log_diff, axis =1)    
+        v = tf.clip_by_value(L-score,clip_value_min=0,clip_value_max=v_max)
+    grad=tape.gradient(v,input_)
+    return v,grad
+
+
 def compute_V_tf(model, input_, target_class):
     logits = model(input_) 
     val, ind= tf.math.top_k(logits,k=2)
     output=val[:,0]-val[:,1]
+    ind = tf.cast(ind,dtype = tf.int32)
     mask=tf.equal(ind[:,0],target_class)
     v=tf.where(condition=mask, x=output,y=tf.zeros(output.shape))
     return v 
+
+def compute_V_tf2(model, input_, target_class,L=0,v_max=1e5):
+    y= model(input_) 
+    y_0 =target_class
+
+    log_diff = tf.concat((y[:,:y_0], y[:,(y_0+1):]),axis=1)-tf.expand_dims(y[:,y_0],-1)
+    score = tf.reduce_max(log_diff, axis =1)
+    return tf.clip_by_value(L-score,clip_value_min=0,clip_value_max=v_max)
 
 def compute_h_tf(model, input_, target_class):
     logits = model(input_) 
@@ -42,4 +70,3 @@ def compute_h_tf(model, input_, target_class):
     return v 
 
 
-norm_dist_tf = tfp.distributions.Normal
