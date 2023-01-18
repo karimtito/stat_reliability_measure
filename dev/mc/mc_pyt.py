e6086eb7ebe7ead9d1125318c8860bd03c2605d9
karimtito <karim.tit@ensae-paristech.fr>
2023-01-18 10:51:19 +0100

    fix MC score condition

--- dev/mc/mc_pyt.py
+++ dev/mc/mc_pyt.py
@@ -18,19 +18,19 @@ def MC_pf(gen,score,N_mc:int=int(1e4),batch_size:int=int(1e2),track_advs:bool=Fa
     x_advs=[]
     for _ in range(N_mc//batch_size):
         x_mc = gen(batch_size)
-        score_MC = score(x=x_mc)
+        score_MC = score(x_mc)
         if track_advs:
             x_advs.append(x_mc[score_MC>=0])
         N+= batch_size
-        p_f = ((N-batch_size)/N)*p_f+(batch_size/N)*(score_MC>0).float().mean()
+        p_f = ((N-batch_size)/N)*p_f+(batch_size/N)*(score_MC>=0).float().mean()
     if N_mc%batch_size!=0:
         rest = N_mc%batch_size
         x_mc = gen(rest)
-        score_MC = score(x=x_mc)
+        score_MC = score(x_mc)
         if track_advs:
             x_advs.append(x_mc[score_MC>=0])
         N+= rest
-        p_f = ((N-rest)/N)*p_f+(rest/N)*(score_MC>0).float().mean()
+        p_f = ((N-rest)/N)*p_f+(rest/N)*(score_MC>=0).float().mean()
     assert N==N_mc
     if track_advs:
         return p_f, x_advs
