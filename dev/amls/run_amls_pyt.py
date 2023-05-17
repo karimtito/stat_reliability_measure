d98c52616c7a8fb5d9dfd69b98e9fc8b13bde774
karimtito <karim.tit@ensae-paristech.fr>
2023-05-17 14:58:35 +0200

    fix type

--- dev/amls/run_amls_pyt.py
+++ dev/amls/run_amls_pyt.py
@@ -21,7 +21,7 @@ str2intList=lambda x: str2list(in_str=x, type_out=int)
 low_str=lambda x: str(x).lower()
 
 
-def run_amls_exp(model, X, y, epsilons=None, dataset_name = None,model_name=None, x_min=0,x_max=None, mask_opt=False,mask_vale=0,
+def run_amls_exp(model, X, y, epsilons=None, dataset_name = None,model_name=None, x_min=0,x_max=None, mask_opt=False,mask_val=0,
                  log_hist_=False, agrr_res_path=None, 
                  log_txt_=False,dict_arg=None,config=None):
     if config is None:
