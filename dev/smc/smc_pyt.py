dd77615a808248f50d05805947f5ff4e5af60f86
karimtito <karim.tit@inria.fr>
2023-06-22 11:44:09 +0200

    add method config class

--- dev/smc/smc_pyt.py
+++ dev/smc/smc_pyt.py
@@ -1,10 +1,12 @@
 import torch 
 import math
 import numpy as np
+from stat_reliability_measure.dev.utils import str2bool,str2floatList,str2intList,float_to_file_float
 from stat_reliability_measure.dev.torch_utils import TimeStepPyt, adapt_verlet_mcmc, adapt_verlet_mcmc2, apply_gaussian_kernel,verlet_mcmc
-
-
-
+from stat_reliability_measure.dev.utils import pars_type, range_vars, simple_vars
+from stat_reliability_measure.config import Config
+import itertools
+from stat_reliability_measure.dev.torch_utils import V_pyt, gradV_pyt
 def dichotomic_search_d(f, a, b, thresh=0, n_max =50):
     """Implementation of dichotomic search of minimum solution for an decreasing function
         Args:
@@ -12,7 +14,6 @@ def dichotomic_search_d(f, a, b, thresh=0, n_max =50):
             -a: lower bound of search space
             -b: upper bound of search space
             -thresh: threshold such that if f(x)>=thresh, x is considered to be a solution of the problem
-    
     """
     low = a
     high = b
@@ -137,10 +138,9 @@ track_accept=False,track_beta=False,return_log_p=False,gaussian=False,
 adapt_dt=False,
 track_calls=True,track_dt=False,track_H=False,track_v_means=False,track_ratios=False,
 target_accept=0.574,accept_spread=0.1,dt_decay=0.999,dt_gain=None,
-dt_min=1e-5,dt_max=1e-2,v_min_opt=True,lambda_0=1,
-debug=False,kappa_opt=False,
+dt_min=1e-5,dt_max=1e-2,v_min_opt=True,kappa_opt=False,
  track_ess=False,M_opt=False,adapt_step=False,alpha_p=0.1,FT=False,sig_dt=0.015,L_min=1,only_duplicated=False,
- GK_opt=False,GV_opt=False,dt_d=1,skip_mh=False):
+ GK_opt=False,GV_opt=False,dt_d=1,skip_mh=False,**kwargs):
     """
       Adaptive SMC estimator with transition kernels either based on:
       underdamped Langevin dynamics  (L=1) or Hamiltonian dynamics (L>1) kernels (GK_opt=False)
@@ -169,6 +169,8 @@ debug=False,kappa_opt=False,
     """
     assert v_min_opt ,"v_min_opt must be True, otherwise all the samples can be killed at the same time"
     gradient_use= not (GK_opt or GV_opt)
+    #assert method_config.adapt_func.lower() in smc_pyt.supported_beta_adapt.keys(),f"select adaptive function in {smc_pyt.supported_beta_adapt.keys}"
+    #adapt_func=smc_pyt.supported_beta_adapt[method_config.adapt_func.lower()]
     #cpu/gpu switch
     if device is None: 
         device= "cuda:0" if torch.cuda.is_available() else "cpu"
@@ -394,6 +396,7 @@ debug=False,kappa_opt=False,
         dic_out['dt_stds']=dt_stds
     return P_est,dic_out
 
+
 def SamplerSmcMulti(gen,  V, gradV,adapt_func,min_rate=0.8,alpha =0.1,N=300,T = 1,L=1,n_max=5000, 
 max_beta=1e6, verbose=False,device=None,
 track_accept=False,track_beta=False,return_log_p=False,gaussian=False,
@@ -433,12 +436,9 @@ debug=False,kappa_opt=False,
     #cpu/gpu switch
     if device is None: 
         device= "cuda:0" if torch.cuda.is_available() else "cpu"
-
-    
     # adaptative parameters
     if adapt_dt and dt_gain is None:
         dt_gain= 1/dt_decay
-
     mcmc_func=verlet_mcmc if not adapt_step else adapt_verlet_mcmc
     #Initializing failure probability
     g_prod=1
@@ -580,11 +580,6 @@ debug=False,kappa_opt=False,
             break
         else:
             G = torch.exp(-(beta-beta_old)*v) #computes current value fonction
-            
-            
-            
-            
-
         g_iter=G.mean()
         g_prod*=g_iter
         if verbose>=0.5:
@@ -627,6 +622,166 @@ debug=False,kappa_opt=False,
     return P_est,dic_out
 
 
+class SMCSamplerConfig(Config):
+    default_dict={'N_range': [32,64,128,256,512,1024],
+                 'T_range':[1,5,10,20,50],
+                  'ess_alpha_range':[0.85],
+    'alpha_range':[],
+    'alpha':0.25,
+    'L_range':[],
+    'ess_alpha':0.875,
+    'L':1,
+    'GV_opt':False,
+     }
+    
+    default_dict.update({'min_rate':0.15,'T':1,
+    'N':100,
+    
+    'n_max':10000,
+    'track_accept':True,
+    'track_calls':True,
+    'mh_opt':False,
+    'adapt_dt':False,
+    'adapt_dt_mcmc':False,
+    'target_accept':0.574,
+    'accept_spread':0.1,
+    'dt_decay':0.999,
+    'dt_gain':-1.,
+    'dt_min':1e-5,
+    'dt_max':0.7,
+    'v_min_opt':True,
+    'ess_opt':False,
+    'only_duplicated':True,
+    'lambda_0':0.5,
+    's_opt':False,
+    's':1.,
+    'clip_s':True,
+    's_min':1e-3,
+    's_max':3.,
+    's_decay':0.95,
+    's_gain':1.0001,
+    'track_ess':True,
+    'track_beta':True,
+    'track_dt':True,
+    'track_v_means':True,
+    'track_ratios':False,
+    'kappa_opt':True,
+    'adapt_func':"ESS",
+    'M_opt':False,
+    'adapt_step':True,
+    'FT':True,
+    'sig_dt':0.02,
+    'L_min':1,
+    'GK_opt':False,
+    'GV_opt':False,
+    'g_target':0.8,
+    'skip_mh':False,
+    'killing':True})
+        
+    def __init__(self,config_dict=default_dict):
+        vars(self).update(config_dict)
+        super().__init__()
+    
+    def add_parsargs(self,parser):
+        for key in vars(self).keys():
+            if ('_range' in key) or  ('_list' in key):
+                # if the key is a range or list and the value is empty 
+                # then the type is the type of the first element of the list
+                if len(vars(self)[key])==0:
+                    ptype = pars_type([vars(self)[key.replace('_range','').replace('_list','')]])
+                else:
+                    ptype = pars_type(vars(self)[key])
+            elif 'pars' in key or 'config' in key:
+                continue
+            else:
+                # else the type is the type of the default value
+                ptype = pars_type(vars(self)[key])
+            parser.add_argument('--'+key,type=ptype,default=vars(self)[key])
+        
+        # parser.add_argument('--N',type=int,default=self.N)
+        # parser.add_argument('--min_rate',type=float,default=self.min_rate)
+        # parser.add_argument('--alpha',type=float,default=self.alpha)
+        # parser.add_argument('--n_max',type=int,default=self.n_max)
+        # parser.add_argument('--ess_alpha',type=float,default=self.ess_alpha)
+        # parser.add_argument('--e_range','--ess_range',type=str2floatList,default=self.e_range)
+        # parser.add_argument('--N_range',type=str2intList,default=self.N_range)
+        # parser.add_argument('--T',type=int,default=self.T)
+        # parser.add_argument('--T_range',type=str2intList,default=self.T_range)
+        # parser.add_argument('--L',type=int,default=self.L)
+        # parser.add_argument('--L_range',type=str2intList,default=self.L_range)
+        # parser.add_argument('--alpha_range',type=str2floatList,default=self.alpha_range)
+        # parser.add_argument('--v1_kernel',type=str2bool,default=self.v1_kernel)
+        # parser.add_argument('--track_accept',type=str2bool,default=self.track_accept)
+        # parser.add_argument('--track_calls',type=str2bool,default=self.track_calls)
+        # parser.add_argument('--track_beta',type=str2bool,default=self.track_beta)
+        # parser.add_argument('--track_ess',type=str2bool,default=self.track_calls)
+        # parser.add_argument('--track_v_means',type=str2bool,default=self.track_v_means)
+        # parser.add_argument('--track_ratios',type=str2bool,default=self.track_ratios)
+        # parser.add_argument('--mh_opt',type=str2bool,default=self.mh_opt)
+        # parser.add_argument('--adapt_dt',type=str2bool,default=self.adapt_dt)
+        # parser.add_argument('--target_accept',type=float,default=self.target_accept)
+        # parser.add_argument('--accept_spread',type=float,default=self.accept_spread)
+        # parser.add_argument('--dt_decay',type=float,default=self.dt_decay)
+        # parser.add_argument('--dt_gain',type=float,default=self.dt_gain)
+        # parser.add_argument('--dt_min',type=float,default=self.dt_min)
+        # parser.add_argument('--dt_max',type=float,default=self.dt_max)
+        # parser.add_argument('--adapt_dt_mcmc',type=str2bool,default=self.adapt_dt_mcmc)
+        # parser.add_argument('--v_min_opt',type=str2bool,default=self.v_min_opt)
+        # parser.add_argument('--ess_opt',type=str2bool,default=self.ess_opt)
+        # parser.add_argument('--lambda_0',type=float,default=self.lambda_0)
+        # parser.add_argument('--track_dt',type=str2bool,default=self.track_dt)
+        # parser.add_argument('--adapt_func',type=str,default=self.adapt_func)
+        # parser.add_argument('--M_opt',type=str2bool,default=self.M_opt)
+        # parser.add_argument('--adapt_step',type=str2bool,default=self.adapt_step)
+        # parser.add_argument('--FT',type=str2bool,default=self.FT)
+        # parser.add_argument('--sig_dt', type=float,default=self.sig_dt)
+        # parser.add_argument('--L_min',type=int,default=self.L_min)
+        # parser.add_argument('--GK_opt',type=str2bool,default=self.GK_opt)
+        # parser.add_argument('--GV_opt',type=str2bool,default=self.GV_opt)
+        # parser.add_argument('--skip_mh',type=str2bool,default=self.skip_mh)
+        # parser.add_argument('--g_target',type=float,default=self.g_target)
+        # parser.add_argument('--kappa_opt',type=str2bool,default=self.kappa_opt)
+        # parser.add_argument('--only_duplicated',type=str2bool,default=self.only_duplicated)
+        return parser
+    
+    def update(self):
+        if len(self.ess_alpha_range)==0:
+            self.ess_alpa_range= [self.ess_alpha]
+        if len(self.N_range)==0:
+            self.N_range= [self.N]
+        if len(self.T_range)==0:
+            self.T_range= [self.T]
+        if len(self.L_range)==0:
+            self.L_range= [self.L]
+        if len(self.alpha_range)==0:
+            self.alpha_range= [self.alpha]
+
+        self.adapt_func_name=self.adapt_func
+        if self.adapt_func.lower()=='simp_ess':
+            self.adapt_func = lambda beta,v : nextBetaSimpESS(beta_old=beta,v=v,lambda_0=self.lambda_0,max_beta=1e6)
+        elif self.adapt_func.lower()=='simp':
+            self.adapt_func = lambda beta,v: SimpAdaptBetaPyt(beta,v,self.g_target,v_min_opt=self.v_min_opt)
+        elif self.adapt_func.lower()=='ess':
+            self.adapt_func = lambda beta,v : nextBetaESS(beta_old=beta,v=v,ess_alpha=self.ess_alpha,max_beta=1e6)
+        if self.GV_opt:
+            self.method_name="RW_SMC"
+        elif self.L==1:
+            self.method_name="MALA_SMC"
+        else:
+            self.method_name="H_SMC"
+        if self.dt_gain==-1:
+            self.dt_gain=1/self.dt_decay
+    def V_classif(self,X):
+        assert hasattr(self,'exp_config'), "exp_config not defined"
+        return V_pyt(X,x_0=self.exp_config.x_0,model=self.exp_config.model,low=self.exp_config.low,high=self.exp_config.high,target_class=self.exp_config.y_0
+                     ,gaussian_latent=self.exp_config.gaussian_latent,noise_dist=self.exp_config.noise_dist,noise_scale=self.exp_config.noise_scale)
+
+
+            
+    def gradV_classif(self,X):
+        assert hasattr(self,'exp_config'), "exp_config not defined"
+        return gradV_pyt(X,x_0=self.exp_config.x_0,model=self.exp_config.model,low=self.exp_config.low,high=self.exp_config.high,target_class=self.exp_config.y_0
+                     ,gaussian_latent=self.exp_config.gaussian_latent,noise_dist=self.exp_config.noise_dist,noise_scale=self.exp_config.noise_scale)
 
 
 
