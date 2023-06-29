6bb7dec030874b525694e19d7ec355802bf33cf7
karimtito <karim.tit@inria.fr>
2023-06-29 11:12:51 +0200

    update MLS config

--- dev/amls/amls_utils.py
+++ dev/amls/amls_utils.py
@@ -3,21 +3,21 @@ import numpy as np
 from stat_reliability_measure.config import SamplerConfig
 
 class MLS_SMC_Config(SamplerConfig):
-    default_dict = { 'method_name': "MLS_SMC",
+    default_dict = { 'config_name': "AMLS",
+        'method_name': "MLS_SMC",
+
                     'N_range':[32,64,128,256,512,1024],'N':40,'T':1,
                     'T_range':[1,10,20,50,100,200,500,1000],
                     'ratio_range':[0.1],
                     'verbose':0,'min_rate':0.51,'clip_s':True,'s_min':8e-3,'s_max':3,
                     'n_max':2000,'allow_zero_est':True,
-                    
-                    'ratio':0,
+                    'ratio':0.1,
                     'n_max':2000,
-                    's':1,
+                    's':1,'track_finish':False,
+                    'track_accept':False,
                     's_range': [1.], 
                     'n_max':2000,
-                
                     'allow_zero_est':True,
-                
                     'last_particle':False}
     def __init__(self, config_dict=default_dict, **kwargs):
         vars(self).update(config_dict)
@@ -27,18 +27,17 @@ class MLS_SMC_Config(SamplerConfig):
 
    
 
-    def update_config(self):
+    def update(self):
         if len(self.N_range)==0:
             self.N_range=[self.N]
-        nb_runs*=len(self.N_range)
+      
         if len(self.T_range)==0:
             self.T_range=[self.T]
-        nb_runs*=len(self.T_range)
+        
         if len(self.ratio_range)==0:
             self.ratio_range=[self.ratio]
-        nb_runs*=len(self.ratio_range)
+        
         if len(self.s_range)==0:
             self.s_range=[self.s]
-        nb_runs*=len(self.s_range)
-        
+        self.K=int(self.N*self.ratio)
         return 
