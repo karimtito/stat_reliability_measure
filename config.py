ffc2fab742a07c472ca55fab2b3087f029cb6b8e
karimtito <karim.tit@inria.fr>
2023-09-13 14:12:17 +0200

    update toy and model config

--- config.py
+++ config.py
@@ -15,7 +15,6 @@ from stat_reliability_measure.dev.utils import float_to_file_float
 
 
 class Config:
-
     def __init__(self,config_dict={}):
         self.config_name='default'
         if len(config_dict)> 0:
@@ -102,7 +101,7 @@ class ExpConfig(Config):
                   'tqdm_opt':True,'clip_min':0.,
                   'clip_max':1.,'force_train':False,
                   'repeat_exp':False,'data_dir':ROOT_DIR+"/data",
-                  'noise_scale':0.1,'exp_name':'',
+                  'noise_scale':1.,'exp_name':'',
                   'notebook':False,}
 
     def __init__(self,config_dict=default_dict):   
@@ -134,92 +133,105 @@ class ExpConfig(Config):
             import cpuinfo
             self.cpu_name=cpuinfo.get_cpu_info()[[key for key in cpuinfo.get_cpu_info().keys() if 'brand' in key][0]]
             self.cores_number=os.cpu_count()
-    
-class Exp1config(ExpConfig):
-    default_dict={'config_name':'Experiment 2',
-                'log_dir':ROOT_DIR+"logs/exp_2_mnist",
-                
-                
-                }
+
+score_functions = {'linear':t_u.build_h_lin,'quadratic':t_u.build_h_quad,'cosinus':t_u.build_h_cos,}
+grad_score_functions = {'linear':t_u.build_gradh_lin,'quadratic':t_u.build_gradh_quad,'cosinus':t_u.build_gradh_cos,}
+V_functions = {'linear':t_u.build_V_lin,'quadratic':t_u.build_V_quad,'cosinus':t_u.build_V_cos,}
+gradV_functions = {'linear':t_u.build_gradV_lin,'quadratic':t_u.build_gradV_quad,'cosinus':t_u.build_gradV_cos,}
+
+class ExpToyConfig(ExpConfig):
+    default_dict={'config_name':'ToyExperimentConfig','dim':2,
+                'log_dir':ROOT_DIR+"/logs/toy_exp",'n_rep':100,
+                'noise_dist':'gaussian','noise_dist':'gaussian',
+                'p_target_range':[],'p_target':1e-3,
+                'score_name':'linear'}
     #p_ref_compute = False
-    def __init__(self,config_dict=default_dict):   
+    def __init__(self,config_dict=default_dict,p_target_range=[],score_name='',
+                aggr_res_path='',verbose=0.,**kwargs):
+       
         vars(self).update(config_dict)
+        if len(score_name)>0:
+            self.score_name=score_name
+        self.p_target_range=p_target_range
+        self.aggr_res_path=aggr_res_path
+        self.verbose=verbose
+        vars(self).update(kwargs)
         super().__init__()
-    def update(self):
+
+    def update(self, method_name=''):
+        super().update()
+        
+        self.method_name = method_name
         self.commit= git.Repo(path=ROOT_DIR).head.commit.hexsha
-        if len(self.model_dir)==0:
-            self.model_dir=os.path.join(ROOT_DIR+"/models/",self.dataset)
-
-        if len(self.model_arch)==0:
-            self.model_arch = t_u.datasets_default_arch[self.dataset]
-
-        if not os.path.exists(self.model_dir):
-            os.mkdir(self.model_dir)
-        
-        self.d = t_u.datasets_dims[self.dataset]
-        #color_dataset=self.dataset in ('cifar10','cifar100','imagenet')
-        #prblm_str=self.dataset
-        if self.input_stop==-1:
-            self.input_stop=self.input_start+1
+        if len(self.p_target_range)==0:
+            if hasattr(self,'p_target'):
+                self.p_target_range=[self.p_target]
+            else:
+                self.p_target_range=[1e-3]
         else:
-            assert self.input_start<self.input_stop,"/!\ input start must be strictly lower than input stop"
-
-        if len(self.noise_dist)==0:
-            self.noise_dist=self.noise_dist.lower()
-
-        if self.noise_dist not in ['uniform','gaussian']:
-            raise NotImplementedError("Only uniform and Gaussian noise distributions are implemented.")
-        
+            self.p_target_range = [float(p) for p in self.p_target_range]
+     
         if len(self.device)==0:
             self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-            if self.verbose>=1:
-                print(f"PyTorch running on device: {self.device}")
+            if self.verbose>=5:
+                print(self.device)
+
         if len(self.log_dir)==0:
-            self.log_dir=os.path.join(ROOT_DIR+'/logs','exp_2_'+self.dataset)
+            self.log_dir=os.path.join(ROOT_DIR+'/logs','toy_exp_'+self.score_name)
         if not os.path.exists(ROOT_DIR+'/logs'):
             os.mkdir(ROOT_DIR+'/logs')  
         if not os.path.exists(self.log_dir):
             os.mkdir(self.log_dir)
         if len(self.aggr_res_path)==0:
             self.aggr_res_path=os.path.join(self.log_dir,'aggr_res.csv')
+       
         self.raw_logs = os.path.join(self.log_dir,'raw_logs/')
         if not os.path.exists(self.raw_logs):
             os.mkdir(self.raw_logs)
-        if len(self.epsilon_range)==0:
-            log_min,log_max=np.log(self.eps_min),np.log(self.eps_max)
-            log_line=np.linspace(start=log_min,stop=log_max,num=self.eps_num)
-            self.epsilon_range=np.exp(log_line)
-        else:
-            self.eps_num=len(self.epsilon_range)
-            self.epsilon_range = [float(eps) for eps in self.epsilon_range]
-            self.eps_min,self.eps_max=np.min(self.epsilon_range),np.max(self.epsilon_range)
+        raw_logs_path=os.path.join(self.log_dir,'raw_logs/'+self.method_name)
+        if not os.path.exists(raw_logs_path):
+            os.mkdir(raw_logs_path)
+        self.loc_time= datetime.today().isoformat().split('.')[0].replace('-','_').replace(':','_')
         
-        test_loader = t_u.get_loader(train=False,data_dir=self.data_dir,download=self.download
-        ,dataset=self.dataset,batch_size=self.load_batch_size,
-            x_mean=None,x_std=None)
-        model, self.model_shape,self.model_name=t_u.get_model(self.model_arch, robust_model=self.robust_model, robust_eps=self.robust_eps,
-            nb_epochs=self.nb_epochs,model_dir=self.model_dir,data_dir=self.data_dir,test_loader=test_loader,device=self.device,
-            download=self.download,dataset=self.dataset, force_train=self.force_train,
-            )
-        self.num_classes=t_u.datasets_num_c[self.dataset.lower()]
-        self.x_mean=t_u.datasets_means[self.dataset]
-        self.x_std=t_u.datasets_stds[self.dataset]
-
-        X_correct,label_correct,accuracy=t_u.get_correct_x_y(data_loader=test_loader,device=self.device,model=model)
-        if self.verbose>=2:
-            print(f"model accuracy on test batch:{accuracy}")
-        if self.use_attack:
-            import foolbox as fb
-            fmodel = fb.PyTorchModel(self.model, bounds=(0,1),device=self.device)
-            attack=fb.attacks.LinfPGD()
-            #un-normalize data before performing attack
-            _, advs, self.attack_success = attack(fmodel, X_correct[self.input_start:self.input_stop], 
-            label_correct[self.input_start:self.input_stop], epsilon_range=self.epsilon_range)
-        inp_indices=np.arange(start=self.input_start,stop=self.input_stop)
-        self.nb_inputs=len(inp_indices)
-        X_test,y_test = X_correct[inp_indices],label_correct[inp_indices]
-
-        return X_test,y_test,model
+        log_name=self.method_name+'_'+self.loc_time
+        exp_log_path=os.path.join(raw_logs_path,log_name)
+        log_path_prop = exp_log_path
+        k=1
+        index_level=0
+        while os.path.exists(log_path_prop):
+            if k==10: 
+                exp_log_path= log_path_prop.replace('9','1')
+                k=1
+                index_level+=1
+            log_path_prop = exp_log_path+'_'+str(k) 
+            if index_level >5:
+                raise RuntimeError("Error the log naming system has failed")
+            k+=1 
+        self.exp_log_path =  log_path_prop                  
+        os.mkdir(path=exp_log_path)
+        self.d= self.dim
+        
+        if self.noise_dist=='gaussian':
+            self.gen= lambda N: torch.randn(size=(N,self.d),device=torch.device(self.device))
+        elif self.noise_dist=='uniform':
+            self.gen = lambda N: (2*torch.rand(size=(N,self.d), device=torch.device(self.device) )-1)
+           
+        self.normal_dist = torch.distributions.normal.Normal(loc=0.,scale=1.)
+        self.V,_ = V_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.gradV,_ = gradV_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.h,self.tresh = score_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.grad_h = grad_score_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.x_clean = torch.zeros(size=(self.dim,),device=torch.device(self.device))
+         
+        return 
+    
+    def update_scores(self):
+        self.V,_ = V_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.gradV,_ = gradV_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.h,self.thresh = score_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        self.grad_h = grad_score_functions[self.score_name](p_target=self.p_target, device=self.device, dim=self.dim, verbose=self.verbose)
+        return
+    
 
     
 class Exp2Config(ExpConfig):
@@ -337,11 +349,8 @@ class Exp2Config(ExpConfig):
             if (not hasattr(self,"model_name")) or self.model_name=='':
                 self.model_name = self.dataset + "_model"
             if (not hasattr(self,"model_arch")) or self.model_arch=='':
-                
                 self.model_arch = 'custom'
-        
-            
-        
+
         self.num_classes=t_u.datasets_num_c[self.dataset.lower()]
         self.x_mean=t_u.datasets_means[self.dataset]
         self.x_std=t_u.datasets_stds[self.dataset]
@@ -371,6 +380,7 @@ class Exp2Config(ExpConfig):
         else:
             self.gen = lambda N: (2*torch.rand(size=(N,self.d), device=torch.device(self.device) )-1)
         self.x_clean=self.X[0]
+        self.input_shape = self.x_clean.shape
         self.y_clean=self.y[0]
         self.low=torch.max(self.x_clean-self.epsilon, torch.tensor([self.x_min]).to(self.device))
         self.high=torch.min(self.x_clean+self.epsilon, torch.tensor([self.x_max]).to(self.device))
@@ -388,6 +398,7 @@ class Exp2Config(ExpConfig):
                     self.high[self.mask_idx]=self.x_clean[self.mask_idx]
             except AttributeError:
                 pass
+        self.normal_dist = torch.distributions.normal.Normal(loc=0.,scale=1.)
         return 
     
     def score(self,x):
@@ -395,6 +406,10 @@ class Exp2Config(ExpConfig):
             y_diff = torch.cat((y[:,:self.y_clean], y[:,(self.y_clean+1):]),dim=1) - y[:,self.y_clean].unsqueeze(-1)
             y_diff, _ = y_diff.max(dim=1)
             return y_diff #.max(dim=1)
+    
+    def gaussian_to_image(self, gaussian_sample, ):
+        image = self.low+(self.high-self.low)*self.normal_dist.cdf(gaussian_sample).view(-1,*self.input_shape)
+        return image
     
     def h(self,X):
         return t_u.h_pyt(X,x_clean=self.x_clean,model=self.model,low=self.low,high=self.high,target_class=self.y_clean
@@ -408,6 +423,7 @@ class Exp2Config(ExpConfig):
         return t_u.gradV_pyt(X,self.x_clean,model=self.model,low=self.low,high=self.high,target_class=self.y_clean
                 ,gaussian_latent=self.gaussian_latent,noise_dist=self.noise_dist,
                 noise_scale=self.noise_scale)
+    
 
 
 
