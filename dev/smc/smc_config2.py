8e0bc31cfc618aa3576c98dd55dc4718276be9f7
karimtito <karim.tit@inria.fr>
2023-11-19 19:24:03 +0100

    add importance exp results

--- dev/smc/smc_config2.py
+++ dev/smc/smc_config2.py
@@ -1,99 +1,9 @@
 from stat_reliability_measure.config import Config
 from stat_reliability_measure.dev.utils import valid_pars_type
 from stat_reliability_measure.dev.smc.smc_utils import nextBetaESS,nextBetaSimpESS,SimpAdaptBetaPyt
-from stat_reliability_measure.home import ROOT_DIR
-from stat_reliability_measure.dev.utils import simple_vars
-
-class config_smc:
-    
-    n_rep=200
-    GV_opt=False
-    L=1
-    e_range=[0.75,0.85,0.95]
-    p_range=[1e-6,1e-12]
-    N_range=[32,64,128,256,512,1024]
-    T_range=[10,20,50]
-    only_duplicated=True
-    v_min_opt=True
-    L_range=[]
-    min_rate=0.15
-    alpha=0.2
-    alpha_range=[]
-    ess_alpha=0.8
-    
-    p_t=1e-6
-    
-    N=100
-    T=1
-    save_config=False 
-    print_config=True
-    d=1024
-    verbose=0
-    log_dir=ROOT_DIR+'/logs/linear_gaussian_tests'
-    aggr_res_path = None
-    update_agg_res=True
-    sigma=1
-    v1_kernel=True
-    torch_seed=None
-    gpu_name=None
-    cpu_name=None
-    cores_number=None
-    track_gpu=False
-    track_cpu=False
-    device=None
-    n_max=10000 
-    allow_multi_gpu=False
-    tqdm_opt=True
-    allow_zero_est=True
-    track_accept=True
-    track_calls=False
-    mh_opt=False
-    adapt_dt=False
-    adapt_dt_mcmc=False
-    target_accept=0.574
-    accept_spread=0.1
-    dt_decay=0.999
-    dt_gain=None
-    dt_min=1e-3
-    dt_max=0.5
-    v_min_opt=True
-    ess_opt=False
-    only_duplicated=True
-    np_seed=None
-    lambda_0=0.5
-    test2=False
-
-    s_opt=False
-    s=1
-    clip_s=True
-    s_min=1e-3
-    s_max=3
-    s_decay=0.95
-    s_gain=1.0001
-
-    track_dt=False
-    mult_last=True
-    linear=True
-
-    track_ess=True
-    track_beta=True
-    track_dt=True
-    track_v_means=True
-    track_ratios=False
-
-    kappa_opt=True
-
-    adapt_func='ESS'
-    M_opt = False
-    adapt_step=True
-    FT=True
-    sig_dt=0.02
-    L_min=1
-    skip_mh=False
 
 
-
-class SMCSamplerConfig2(Config):
+class SMCSamplerConfig(Config):
     default_dict={"config_name":"SMC",
         "method_name":'SMC',
         'requires_V':True,
@@ -119,7 +29,6 @@ class SMCSamplerConfig2(Config):
     'n_max':10000,
     'track_accept':True,
     'track_calls':True,
-    'track_X':False,
     'mh_opt':False,
     'adapt_dt':False,
     'adapt_dt_mcmc':False,
@@ -138,7 +47,6 @@ class SMCSamplerConfig2(Config):
     'track_dt':True,
     'track_v_means':True,
     'track_ratios':False,
-    'track_advs':False,
     'kappa_opt':True,
     'adapt_func':"ESS",
     'M_opt':False,
@@ -153,7 +61,7 @@ class SMCSamplerConfig2(Config):
         
     def __init__(self,config_dict=default_dict):
         vars(self).update(config_dict)
-        vars(self).update(simple_vars(config_smc))
+        
         super().__init__()
    
             
@@ -179,7 +87,7 @@ class SMCSamplerConfig2(Config):
     
     def update(self):
     
-    
+
         if len(self.ess_alpha_range)==0:
             self.ess_alpha_range= [self.ess_alpha]
         if len(self.N_range)==0:
