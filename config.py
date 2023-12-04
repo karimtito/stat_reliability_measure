51a1bee6063b0b7a4f6c25f0fdc65c9b3ceaeca4
karimtito <karim.tit@inria.fr>
2023-12-04 13:45:39 +0100

    update config

--- config.py
+++ config.py
@@ -250,18 +250,18 @@ class ExpToyConfig(ExpConfig):
 class ExpModelConfig(ExpConfig):
     default_dict={'config_name':'ModelExperimentConfig','dataset':'',
                   'data_dir':ROOT_DIR+"/data",'model':None,
-                'log_dir':"",'low':None,'high':None,
+                'log_dir':"",'low':None,'high':None,'simga_noise':1.,
                 'model_arch':'','model_dir':'','epsilon_range':[],'eps_max':0.3,'eps_min':0.2,
                 'eps_num':5,'epsilon':-1.,'input_start':0,'input_stop':-1,'n_rep':100,'model_path':'',
                 'export_to_onnx':False,'use_attack':False,'attack':'PGD','X':None,'y':None,
                 'lirpa_bounds':False,'download':True,'train_model_epochs':10,'method_name':'',
-                'from_gaussian':True,'noise_dist':'uniform','x_min':None,'mask_opt':False,
-                'sigma':1.,'x_max':None,'x_mean':None,'x_std':1.,'lirpa_cert':False,'robust_model':False,
+                'from_gaussian':True,'noise_dist':'uniform','x_min':0.,'mask_opt':False,
+                'sigma':1.,'x_max':1.,'x_mean':None,'x_std':1.,'lirpa_cert':False,'robust_model':False,
                 'robust_eps':0.1,'load_batch_size':128,'nb_epochs': 15,'adversarial_every':1,}
     #p_ref_compute = False
     def __init__(self,config_dict=default_dict,X=None,y=None,model=None,epsilon_range=[],
-                dataset_name='mnist',aggr_res_path='',model_name='',model_arch='',verbose=0.,
-                torch_seed=-1,np_seed=-1,random_seed=-1,
+                dataset_name='mnist',aggr_res_path='',model_name='',model_arch='',verbose=0.,input_index=-1 ,
+                torch_seed=-1,np_seed=-1,random_seed=-1,normal_cdf_layer=None,sigma_noise=1.,model_path='',
                 method_name='',shuffle=False,real_uniform=False,x_mean=None,x_std=None,**kwargs):
         super().__init__(torch_seed=torch_seed,np_seed=np_seed,verbose=verbose,random_seed = random_seed,)
         vars(self).update(config_dict)
@@ -270,11 +270,16 @@ class ExpModelConfig(ExpConfig):
         self.model_name= model_name
         self.model_arch = model_arch
         self.model = model
+        self.model_path = model_path
+        
         self.epsilon_range=epsilon_range
         self.dataset=dataset_name
         self.aggr_res_path=aggr_res_path
         self.verbose=verbose
         self.real_uniform=real_uniform
+        self.sigma_noise=sigma_noise    
+        self.input_index=input_index
+        
         vars(self).update(kwargs)
         if hasattr(self,"model_name") and len(self.model_name)>0:
             self.model_arch = self.model_name
@@ -321,7 +326,7 @@ class ExpModelConfig(ExpConfig):
         assert standard_dataset or (data_provided and model_provided), "Either a standard dataset or a model and data must be provided"
         if standard_dataset:
             if self.dataset=='imagenet':
-                self.load_batch_size=64
+                self.load_batch_size= min(self.load_batch_size,256)
             test_loader = t_u.get_loader(train=False,data_dir=self.data_dir,download=self.download
                 ,dataset=self.dataset,batch_size=self.load_batch_size, shuffle=shuffle,
                     x_mean=None,x_std=None)
@@ -335,7 +340,7 @@ class ExpModelConfig(ExpConfig):
                 else:
                     self.model, self.model_shape,self.model_name=t_u.get_model(self.model_arch, robust_model=self.robust_model, robust_eps=self.robust_eps,
                         nb_epochs=self.nb_epochs,model_dir=self.model_dir,data_dir=self.data_dir,test_loader=test_loader,device=self.device,
-                        download=self.download,dataset=self.dataset, force_train=self.force_train,
+                        download=self.download,dataset=self.dataset, force_train=self.force_train,model_path=self.model_path,
                     )
         
             if not data_provided:
@@ -350,17 +355,9 @@ class ExpModelConfig(ExpConfig):
             except KeyError:
                 self.x_mean=None
                 self.x_std =None
-                
-            
-            
-                
-
         else:
-            
             """compute the accuracy of the model on the sample batch"""
             self.sample_accuracy=t_u.get_model_accuracy(model=self.model,X=self.X[self.input_start:self.input_stop],y=self.y[self.input_start:self.input_stop])
-        
-        
         if len(self.epsilon_range)==0:
             log_min,log_max=np.log(self.eps_min),np.log(self.eps_max)
             log_line=np.linspace(start=log_min,stop=log_max,num=self.eps_num)
@@ -390,27 +387,26 @@ class ExpModelConfig(ExpConfig):
         if self.verbose>=5:
             print (f"input indices: {inp_indices}")
         self.nb_inputs=len(inp_indices)
-        
+        if self.input_index==-1:
+            self.input_index=inp_indices[0]
         if self.from_gaussian:
             self.gen= lambda N: torch.randn(size=(N,self.d),device=torch.device(self.device))
         else:
             self.gen = lambda N: (2*torch.rand(size=(N,self.d), device=torch.device(self.device) )-1)
-        self.x_clean=self.X[0]
+        self.x_clean=self.X[self.input_index]
         self.input_shape = self.x_clean.shape
-        self.y_clean=self.y[0]
+        self.y_clean=self.y[self.input_index]
         if self.noise_dist in ('gaussian','normal'):
-            self.normal_cdf_layer = torch.nn.Identity()
-        elif self.noise_dist=='uniform':
-            self.normal_cdf_layer = t_u.NormalCDFLayer(offset=self.x_clean, 
-            epsilon=self.epsilon, 
-            x_min=self.x_min,x_max=self.x_max,device=self.device)
+            self.normal_cdf_layer = t_u.NormalClampReshapeLayer(offset=self.x_clean, sigma=self.sigma_noise,)
+        
         self.build_parser()
 
-    def update(self, method_name=''):
+    def update(self, input_idx=None, method_name=''):
         """ """
         
+        if len(self.method_name)>0:
+            self.method_name = method_name
         
-        self.method_name = method_name
         raw_logs_path=os.path.join(self.log_dir,'raw_logs/'+method_name)
         if not os.path.exists(raw_logs_path):
             os.mkdir(raw_logs_path)
@@ -421,6 +417,12 @@ class ExpModelConfig(ExpConfig):
         log_path_prop = exp_log_path
         k=1
         index_level=0
+        if input_idx is not None:
+            self.input_index = input_idx
+        self.x_clean=self.X[self.input_index]
+        self.y_clean=self.y[self.input_index]
+        print(f"Current data index: {self.input_index}")
+        t_u.plot_tensor(self.x_clean,y=self.y_clean)
         while os.path.exists(log_path_prop):
             if k==10: 
                 exp_log_path= log_path_prop.replace('9','1')
@@ -462,16 +464,20 @@ class ExpModelConfig(ExpConfig):
                     self.high[self.mask_idx]=self.x_clean[self.mask_idx]
             except AttributeError:
                 pass
+            
         if self.noise_dist=='uniform':
             if self.real_uniform:
                 self.normal_cdf_layer= t_u.NormalToUnifLayer( low = self.low, 
-                high = self.high, device=self.device)
+                high = self.high, device=self.device, input_shape = self.input_shape)
                     
                 
             else:
-                self.normal_cdf_layer = t_u.NormalCDFLayer(offset=self.x_clean, 
+                self.normal_cdf_layer = t_u.NormalCDFLayer(x_clean=self.x_clean, 
                 epsilon=self.epsilon, 
                 x_min=self.x_min,x_max=self.x_max,device=self.device)
+        elif self.noise_dist in ['gaussian','normal']:
+            self.normal_cdf_layer = t_u.NormalClampReshapeLayer(offset=self.x_clean, sigma=self.sigma_noise,)
+            
         return 
     
     def score(self,x):
@@ -510,200 +516,25 @@ class ExpModelConfig(ExpConfig):
         return t_u.gradV_pyt(X,self.x_clean,model=self.model,low=self.low,high=self.high,target_class=self.y_clean
                 ,from_gaussian=self.from_gaussian,noise_dist=self.noise_dist,
                 noise_scale=self.noise_scale)
+        
+    def V_alt(self,X):
+        return t_u.V_auto_diff( x_= X,target_class=self.y_clean,T_transform=self.normal_cdf_layer, model=self.model,)
+    
+    def gradV_alt(self,X):
+        return t_u.gradV_auto_diff(x_ = X, target_class=self.y_clean, T_transform=self.normal_cdf_layer, model=self.model,)
+    
+    def h_alt(self,X):
+        return t_u.h_pyt_alt(x_ = X, target_class=self.y_clean, T_transform=self.normal_cdf_layer, model=self.model,)
+    def gradh_alt(self,X):
+        return t_u.gradh_auto_diff(x_ = X, target_class=self.y_clean, T_transform=self.normal_cdf_layer, model=self.model,)
+    def G_alt(self,X):
+        return -self.h_alt(X)
+    def gradG_alt(self,X):
+        return t_u.gradG_auto_diff(x_ = X, target_class=self.y_clean, T_transform=self.normal_cdf_layer, model=self.model,)
+    
     
 
 
-
-class Exp3Config(ExpConfig):
-    default_dict={'config_name':'Experiment 2','dataset':'imagenet',
-                  'data_dir':ROOT_DIR+"/data",
-                'log_dir':ROOT_DIR+"/logs/exp_3_imagenet",
-                'model_arch':'','model_dir':'','epsilon_range':[],'eps_max':0.3,'eps_min':0.2,
-                'eps_num':5,'epsilon':-1.,'input_start':0,'input_stop':-1,'n_rep':100,'model_path':'',
-                'export_to_onnx':False,'use_attack':False,'attack':'PGD',
-                'lirpa_bounds':False,'download':True,'train_model_epochs':10,
-                'from_gaussian':True,'noise_dist':'uniform','x_min':0.,'mask_opt':False,
-                'sigma':1.,'x_max':1.,'x_mean':0.,'x_std':1.,'lirpa_cert':False,'robust_model':False,
-                'robust_eps':0.1,'load_batch_size':128,'nb_epochs': 15,'adversarial_every':1,}
-    #p_ref_compute = False
-    def __init__(self,config_dict=default_dict,X=None,y=None,model=None,epsilon_range=[],
-                dataset_name='mnist',aggr_res_path='',model_name='',verbose=0.,**kwargs):
-        if X is not None:
-            self.X=X
-        if y is not None:
-            self.y=y
-        if model is not None:
-            self.model=model   
-        vars(self).update(config_dict)
-        self.epsilon_range=epsilon_range
-        self.dataset=dataset_name
-        self.aggr_res_path=aggr_res_path
-        self.verbose=verbose
-        vars(self).update(kwargs)
-        super().__init__()
-
-    def update(self, method_name=''):
-        super().update()
-        self.method_name = method_name
-        self.commit= git.Repo(path=ROOT_DIR).head.commit.hexsha
-        if len(self.model_dir)==0:
-            self.model_dir=os.path.join(ROOT_DIR+"/models/",self.dataset)
-        if not os.path.exists(self.model_dir):
-            os.mkdir(self.model_dir)
-        #color_dataset=self.dataset in ('cifar10','cifar100','imagenet')
-        #prblm_str=self.dataset
-        if self.input_stop==-1:
-            self.input_stop=self.input_start+1
-        else:
-            assert self.input_start<self.input_stop,"/!\ input start must be strictly lower than input stop"
-        if len(self.noise_dist)==0:
-            self.noise_dist=self.noise_dist.lower()
-        if self.noise_dist not in ['uniform','gaussian']:
-            raise NotImplementedError("Only uniform and Gaussian distributions are implemented.")
-        # if not self.allow_multi_gpu:
-        #     os.environ["CUDA_VISIBLE_DEVICES"]="0"
-        if len(self.device)==0:
-            self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-            if self.verbose>=5:
-                print(self.device)
-
-
-        if len(self.log_dir)==0:
-            self.log_dir=os.path.join(ROOT_DIR+'/logs','exp_2_'+self.dataset)
-        if not os.path.exists(ROOT_DIR+'/logs'):
-            os.mkdir(ROOT_DIR+'/logs')  
-        if not os.path.exists(self.log_dir):
-            os.mkdir(self.log_dir)
-        if len(self.aggr_res_path)==0:
-            self.aggr_res_path=os.path.join(self.log_dir,'aggr_res.csv')
-       
-        
-        self.raw_logs = os.path.join(self.log_dir,'raw_logs/')
-        if not os.path.exists(self.raw_logs):
-            os.mkdir(self.raw_logs)
-        raw_logs_path=os.path.join(self.log_dir,'raw_logs/'+self.method_name)
-        if not os.path.exists(raw_logs_path):
-            os.mkdir(raw_logs_path)
-        self.loc_time= datetime.today().isoformat().split('.')[0].replace('-','_').replace(':','_')
-        
-        log_name=self.method_name+'_'+self.loc_time
-        exp_log_path=os.path.join(raw_logs_path,log_name)
-        log_path_prop = exp_log_path
-        k=1
-        index_level=0
-        while os.path.exists(log_path_prop):
-            if k==10: 
-                exp_log_path= log_path_prop.replace('9','1')
-                k=1
-                index_level+=1
-            log_path_prop = exp_log_path+'_'+str(k) 
-            if index_level >5:
-                raise RuntimeError("Error the log naming system has failed")
-            k+=1 
-
-        self.exp_log_path =  log_path_prop                  
-        
-        os.mkdir(path=exp_log_path)
-        if len(self.epsilon_range)==0:
-            log_min,log_max=np.log(self.eps_min),np.log(self.eps_max)
-            log_line=np.linspace(start=log_min,stop=log_max,num=self.eps_num)
-            self.epsilon_range=np.exp(log_line)
-        else:
-            self.eps_num=len(self.epsilon_range)
-            self.epsilon_range = [float(eps) for eps in self.epsilon_range]
-            self.eps_min,self.eps_max=np.min(self.epsilon_range),np.max(self.epsilon_range)
-        if self.epsilon==-1.:
-            self.epsilon = self.epsilon_range[0]
-        
-        test_loader = t_u.get_loader(train=False,data_dir=self.data_dir,download=self.download
-        ,dataset=self.dataset,batch_size=self.load_batch_size,
-            x_mean=None,x_std=None)
-        if not hasattr(self,"model"):
-            if len(self.model_arch)==0:
-                self.model_arch = t_u.datasets_default_arch[self.dataset]
-            self.model, self.model_shape,self.model_name=t_u.get_model_imagenet(self.model_arch, 
-                model_dir=self.model_dir,
-                )
-        else:
-            if (not hasattr(self,"model_name")) or self.model_name=='':
-                self.model_name = self.dataset + "_model"
-            if (not hasattr(self,"model_arch")) or self.model_arch=='':
-                
-                self.model_arch = 'custom'
-        
-            
-        
-        self.num_classes=t_u.datasets_num_c[self.dataset.lower()]
-        self.x_mean=t_u.datasets_means[self.dataset]
-        self.x_std=t_u.datasets_stds[self.dataset]
-        if not hasattr(self,"X"):
-            self.X,self.y,self.sample_accuracy=t_u.get_correct_x_y(data_loader=test_loader,
-                device=self.device,model=self.model)
-
-        else:
-            """compute the accuracy of the model on the sample batch"""
-            self.sample_accuracy=t_u.get_model_accuracy(model=self.model,
-            X=self.X[self.input_start:self.input_stop],y=self.y[self.input_start:self.input_stop])
-        self.d= np.prod(self.X.shape[1:])
-        if self.verbose >=1:
-            print(f"model accuracy on sample batch:{self.sample_accuracy}")
-        if self.use_attack:
-            import foolbox as fb
-            fmodel = fb.PyTorchModel(self.model, bounds=(0,1),device=self.device)
-            attack = fb.attacks.LinfPGD()
-            #un-normalize data before performing attack
-            _, advs, self.attack_success = attack(fmodel, self.X[self.input_start:self.input_stop], 
-            self.y[self.input_start:self.input_stop], epsilon_range=self.epsilon_range)
-        inp_indices=np.arange(start=self.input_start,stop=self.input_stop)
-        if self.verbose>=5:
-            print (f"input indices: {inp_indices}")
-        self.nb_inputs=len(inp_indices)
-        
-        if self.from_gaussian:
-            self.gen= lambda N: torch.randn(size=(N,self.d),device=torch.device(self.device))
-        else:
-            self.gen = lambda N: (2*torch.rand(size=(N,self.d), device=torch.device(self.device) )-1)
-        self.x_clean=self.X[0]
-        self.y_clean=self.y[0]
-        self.low=torch.max(self.x_clean-self.epsilon, torch.tensor([self.x_min]).to(self.device))
-        self.high=torch.min(self.x_clean+self.epsilon, torch.tensor([self.x_max]).to(self.device))
-        if self.mask_opt:
-            try:
-                if self.mask_cond is not None:
-                    idx = self.mask_cond(self.x_clean)
-                    self.low[idx]=self.x_clean[idx]
-                    self.high[idx]=self.x_clean[idx]
-            except AttributeError:
-                pass
-            try:
-                if self.mask_idx is not None:
-                    self.low[self.mask_idx]=self.x_clean[self.mask_idx]
-                    self.high[self.mask_idx]=self.x_clean[self.mask_idx]
-            except AttributeError:
-                pass
-        return 
-    
-    def score(self,x):
-            y = self.model(x)
-            y_diff = torch.cat((y[:,:self.y_clean], y[:,(self.y_clean+1):]),dim=1) - y[:,self.y_clean].unsqueeze(-1)
-            y_diff, _ = y_diff.max(dim=1)
-            return y_diff #.max(dim=1)
-    def h(self,X):
-        return t_u.h_pyt(X,x_clean=self.x_clean,model=self.model,low=self.low,high=self.high,
-                         target_class=self.y_clean
-                ,from_gaussian=self.from_gaussian,noise_dist=self.noise_dist,
-                noise_scale=self.noise_scale)
-    def V(self,X):
-        return t_u.V_pyt(X,self.x_clean,model=self.model,low=self.low,high=self.high,
-                         target_class=self.y_clean,
-                from_gaussian=self.from_gaussian,noise_dist=self.noise_dist,
-                noise_scale=self.noise_scale)   
-    def gradV(self,X):
-        return t_u.gradV_pyt(X,self.x_clean,model=self.model,low=self.low,high=self.high,
-                             target_class=self.y_clean,
-                from_gaussian=self.from_gaussian,noise_dist=self.noise_dist,
-                noise_scale=self.noise_scale)
-      
     
     
 class SamplerConfig(Config):
